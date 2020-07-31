13310e9f462b5b7260ecd18d537dda602e36bcc9
Jon <jdog992@gmail.com>
2020-07-31 16:24:53 -0400

    predeploy

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
+  "host": process.env.MIGRATION_DATABASE_HOST,
+  "port": process.env.MIGRATION_DATABASE_PORT,
+  "database": process.env.MIGRATION_DATABASE_NAME,
+  "username": process.env.MIGRATION_DATABASE_USER,
+  "password": process.env.MIGRATION_DATABASE_PASS,
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
